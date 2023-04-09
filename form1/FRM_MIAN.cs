4d9d21960086703b4e432b21d0097b78efddbcf8
Suaad-abduljalil <sabduljalil.mdbf20@iste.edu.tr>
2023-04-09 21:53:54 +0300

    arayüz tasarımı

---
+++ form1/FRM_MIAN.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibM29.PL
+{
+    public partial class FRM_MIAN : Form
+    {
+        public FRM_MIAN()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuImageButton1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void bunifuImageButton3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void bunifuImageButton4_Click(object sender, EventArgs e)
+        {
+            if(WindowState == FormWindowState.Normal)
+              {
+                this.WindowState = FormWindowState.Maximized;
+
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void bunifuImageButton6_Click(object sender, EventArgs e)
+        {
+            if (P_MB.Size.Width==175)
+            {
+                P_MB.Width = 50;
+                button1.RightToLeft = RightToLeft.Yes;
+                button2.RightToLeft = RightToLeft.Yes;
+                button3.RightToLeft = RightToLeft.Yes;
+                button4.RightToLeft = RightToLeft.Yes;
+               
+            }
+            else
+            {
+                P_MB.Width = 175;
+                
+               
+               
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
